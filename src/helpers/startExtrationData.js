aaef77b870d067d54c87749f9419f1f9aee6bf89
Michael-saeek <saeeksitow@gmail.com>
2022-05-14 00:22:03 -0300

    'updatingNewConfigurations'

--- src/helpers/startExtrationData.js
+++ src/helpers/startExtrationData.js
@@ -11,34 +11,49 @@ const startExtrationData = async (dataCheckIn, dataCheckOut, month) => {
             args: ['--no-sandbox'],
         }
     );
-    const page = await browser.newPage(
-        {
-            waitUntil: 'domcontentLoaded'
-        }
-    );
+    const page = await browser.newPage();
+
+        await page.setViewport({
+            width: 1920,
+            height: 1080,
+           
+        });
+
+        await page.setUserAgent('MyBrowser', {
+            architecture: 'My1',
+            mobile: false,
+            model: 'Mybook',
+            platform: 'MyOS',
+            platformVersion: '3.1',
+          });
+    
+    const versionB = await browser.version()
+
+    
 
 
     const urlsNewData = buscarFechasyReemplazarlas(dataCheckIn, dataCheckOut, month, urls)
+
+    console.log(urlsNewData)
+
     try {
         console.clear()
+   
         console.log(`Começando a extração de dados de ${urlsNewData.length} estabelecimentos`)
+  
+
+        console.log(versionB)
 
         for (let i = 0; i < urlsNewData.length; i++) {
-            
-           
-            await page.goto(urlsNewData[i]);
-           /* await page.screenshot({
-                path: `./src/imgs/img${i}.png`,
-                fullPage: true,
-            })
-
-            */
-
-            console.log('')
-            console.log(`Pagina numero ${[i]}`)
-
+            await page.goto(urlsNewData[i], { waitUntil: 'domcontentloaded'});
+            await page.waitForTimeout(2500);
+          //  await page.screenshot({ path: './src/imgs/img1.jpeg', })
         
-            const data = await page.evaluate(() => {    
+            console.log('')
+            console.log(`Pagina: ${urlsNewData[i]}`)
+
+
+            const data = await page.evaluate( async () => {    
                 let textImg = document.querySelector('.bh-photo-grid [data-preview-image-layout] img')?.src
                 let textHotel = document.querySelector('.hp__hotel-title h2')?.innerText
                 let textLocalizacao = document.querySelector('.address .hp_address_subtitle')?.innerText
@@ -48,11 +63,10 @@ const startExtrationData = async (dataCheckIn, dataCheckOut, month) => {
                 let textDiarias = document.querySelector(' [data-component] .sb-dates__los')?.innerText
                 let textPrecototal = document.querySelector('tbody tr .hprt-price-block span')?.innerText
                 
-                function isaNull(selectorDOM) {
-                    const datos = selectorDOM == `TypeError: Cannot read properties of null (reading 'innerText')` || selectorDOM == null ? 'Valor não encontrado' : selectorDOM
-                    return datos
+                function isaNull(selectorDOM) { 
+                    const datos = selectorDOM == `TypeError: Cannot read properties of null (reading 'innerText')` || selectorDOM == null ? 'is a null' : selectorDOM
+                    return datos 
                 }
-
                 return {
                     backgroundImg: isaNull(textImg),
                     hotel: isaNull(textHotel),
@@ -70,11 +84,12 @@ const startExtrationData = async (dataCheckIn, dataCheckOut, month) => {
            console.log(data) 
            console.log(``)
            console.log(`-------------->`)
-            
-        }
        
+        }
+        
 
     } 
+ 
 
     catch (error) {
         console.log(`Este es el error: ${error}`)
@@ -82,6 +97,7 @@ const startExtrationData = async (dataCheckIn, dataCheckOut, month) => {
    
     await browser.close();
     const dataHotel = JSON.stringify(DataResults) 
+  
     return dataHotel
 }
 
